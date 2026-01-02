eab889ac6ed2c085ea13688c10d9f67ea155bd99
sex <yourname@sex.localdomain>
2026-01-03 02:19:53 +0300

    intro screen

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -1,6 +1,13 @@
 export {};
 
 declare global {
+  type TelegramInsets = {
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  };
+
   interface Window {
     Telegram?: {
       WebApp: {
@@ -9,6 +16,10 @@ declare global {
         ready: () => void;
         expand: () => void;
         requestFullscreen?: () => void;
+        contentSafeAreaInset?: TelegramInsets;
+        safeAreaInset?: TelegramInsets;
+        onEvent?: (event: "viewportChanged", callback: () => void) => void;
+        offEvent?: (event: "viewportChanged", callback: () => void) => void;
         close: () => void;
         sendData: (data: string) => void;
       };
